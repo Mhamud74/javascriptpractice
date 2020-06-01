f4fab865bcc3470741f38b976582306a94e155de
Manik <manikmhamud74@outlook.com>
2020-06-01 23:34:49 +0600

    data type

---
+++ data type/javascript.js
@@ -0,0 +1,11 @@
+// primaitive datatype
+
+const name ="Manik";
+console.log(typeof name);
+const age=21;
+console.log(typeof age);
+const hasname=false;
+console.log(typeof hasname);
+let test;
+const sym= Symbol()
+console.log(typeof sym);
