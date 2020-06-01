87ab51cd0498856d1e1ebfbf057baa3f3cc600cb
Manik <manikmhamud74@outlook.com>
2020-06-01 10:52:43 +0600

    variable

--- consol/javascript.js
+++ consol/javascript.js
@@ -1,6 +1,5 @@
 console.log('Hlw World');
 console.log(1234);
 console.log(false);
-
 console.error('First Wraning');
 console.warn('Second Wraning');
---
+++ variable/javascript.js
@@ -0,0 +1,19 @@
+// var let const
+
+// var name="Manik";
+// var age =22;
+// name="Mhamud"
+// console.log(name);
+// console.log(age);
+
+
+// let name="Manik";
+// let age =20;
+// console.log(name);
+// age=44;
+// console.log(age);
+
+const name="Manik";
+const age=23;
+const name1="MMhamud"
+console.log(name);
