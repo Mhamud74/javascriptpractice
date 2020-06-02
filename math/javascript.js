351190c821be68eab087545d46c606d84295afff
Manik <manikmhamud74@outlook.com>
2020-06-02 10:44:25 +0600

    math object and string

---
+++ math/javascript.js
@@ -0,0 +1,25 @@
+const num1 =100;
+const num2=50;
+let val;
+val=num1+num2;
+console.log(val);
+val=num1-num2;
+console.log(val);
+val=num1*num2;
+console.log(val);
+val=num1%num2;
+console.log(val);
+val=num1/num2;
+console.log(val);
+
+
+// math object
+val = Math.PI;
+console.log(val);
+val=Math.round(2.6);
+console.log(val);
+val=Math.sqrt(64);
+console.log(val);
+val = Math.random();
+console.log(val);
+console.log(val);
---
+++ string/javascript.js
@@ -0,0 +1,8 @@
+const firstName = "Manik";
+const secondName ="Mhamud";
+let val;
+val=firstName+" "+secondName;
+val="Hlw Mr "+ firstName+ " How are You";
+val=firstName.length;
+val=firstName.toUpperCase();
+console.log(val);
